049551c12f2bdfbca60becb5b9192d9bf9c3953a
ColdWape <92642047+ColdWape@users.noreply.github.com>
2023-03-05 21:31:50 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+function show_info(){
+    document.getElementById("infoBlock").style.display = 'block'
+    document.getElementById("infoBlock").style.opacity = 1
+}
+
+function hide_info(){
+    document.getElementById("infoBlock").style.opacity = 0
+    document.getElementById("infoBlock").style.display = 'none'
+}
